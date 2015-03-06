7838fa5aa7a7c2bb99888396d89686fd7dc639ca
lijian <hustlijian@gmail.com>
2015-03-06 12:33:17 +0800

    add mds meta table definetion

---
+++ simplecfs/mds/meta_table.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+define meta data table names prefix
+"""
+# table prefix
+DIR_PREFIX = 'dir:'     # directory
+SUB_PREFIX = 'sub:'     # subfiles
+FILE_PREFIX = 'file:'   # file
+OBJ_PREFIX = 'obj:'     # object
+CHK_PREFIX = 'chk:'     # chunk
+DS_PREFIX = 'ds:'       # ds
+
+
+def dir_key(dirname):
+    """use dirname to get dir key"""
+    return DIR_PREFIX + dirname
+
+
+def sub_key(dirname):
+    """use dirname to get subfiles key"""
+    return SUB_PREFIX + dirname
+
+
+def file_key(filename):
+    """use filename to get file key"""
+    return FILE_PREFIX + filename
+
+
+def obj_key(objname):
+    """use object name to get object key"""
+    return OBJ_PREFIX + objname
+
+
+def chk_key(chkname):
+    """use chunk name to get chunk key"""
+    return CHK_PREFIX + chkname
+
+
+def ds_key(ip, port):
+    """use ip and port to get ds key"""
+    return DS_PREFIX + ip + ':' + str(port)
---
+++ tests/mds/test_meta_table.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+unit test for meta tables
+"""
+from nose.tools import eq_
+
+from simplecfs.mds.meta_table import dir_key, sub_key, file_key, obj_key,\
+    chk_key, ds_key
+
+
+def test_dir_key():
+    dirname = '/testdir/'
+    key = dir_key(dirname)
+    eq_(key, 'dir:/testdir/')
+
+
+def test_sub_key():
+    dirname = '/testdir/'
+    key = sub_key(dirname)
+    eq_(key, 'sub:/testdir/')
+
+
+def test_file_key():
+    filename = '/testfile'
+    key = file_key(filename)
+    eq_(key, 'file:/testfile')
+
+
+def test_obj_key():
+    objname = '/test_obj0'
+    key = obj_key(objname)
+    eq_(key, 'obj:/test_obj0')
+
+
+def test_chk_key():
+    chkname = '/test_ob0_chk0'
+    key = chk_key(chkname)
+    eq_(key, 'chk:'+chkname)
+
+
+def test_ds_key():
+    ip = '127.0.0.1'
+    port = 7000
+    key = ds_key(ip, port)
+    eq_(key, 'ds:127.0.0.1:7000')
