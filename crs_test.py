9fd6ae8d9792969ca7f23074368d2e27ff2ff682
lijian <hustlijian@gmail.com>
2015-05-05 13:01:48 +0800

    add crs_test.py for test

--- crs_test.py
+++ crs_test.py
@@ -8,7 +8,7 @@ import ConfigParser as configparser
 import time
 
 from simplecfs.client.api import Client
-from simplecfs.common.parameters import CODE_RS
+from simplecfs.common.parameters import CODE_CRS
 from simplecfs.common.parameters import DS_BROKEN, DS_CONNECTED
 
 des_path = './testM.txt'
@@ -16,10 +16,10 @@ base_path = '/Users/lijian/temp/test/'
 src_path = ''
 
 code_info = {  # default code info
-    'type': CODE_RS,
+    'type': CODE_CRS,
     'k': 3,
     'm': 2,
-    'w': 8,
+    'w': 4,
     'packet_size': 1024,
     'block_size': 33554432,  # 32M
 }
@@ -48,16 +48,16 @@ def init():
 def test_putfile(client):
     t1 = time.time()
     client.putfile(src_path, des_path, code_info)
-    interval = time.time()-t1
+    interval = time.time() - t1
     return interval * 1000  # ms
 
 
 def test_normal_get_chunk(client):
     chunk_id = '/testM.txt_obj0_chk0'
-    local_path = base_path + 'rschk0'
+    local_path = base_path + 'crschk0'
     t1 = time.time()
     client.getchunk(chunk_id, local_path)
-    interval = time.time()-t1
+    interval = time.time() - t1
     return interval * 1000  # ms
 
 
@@ -65,11 +65,11 @@ def test_degrade_get_chunk(client):
     chunk_id = '/testM.txt_obj0_chk0'
     (ip, port) = client.get_chunk_ds_id(chunk_id)
     client.report_ds(ip, port, DS_BROKEN)
-    degrade_path = base_path + 'degrade_rschk0'
+    degrade_path = base_path + 'degrade_crschk0'
 
     t1 = time.time()
     client.getchunk(chunk_id, degrade_path)
-    interval = time.time()-t1
+    interval = time.time() - t1
     client.report_ds(ip, port, DS_CONNECTED)
 
     return interval * 1000  # ms
@@ -90,7 +90,8 @@ if __name__ == '__main__':
         file_name = str(each_num)+'M.txt'
         src_path = base_path+file_name
         file_size = each_num*(1024*1024)
-        code_info['block_size'] = (file_size/(3*1024)+1)*1024
+        r = 4
+        code_info['block_size'] = (file_size/(3*r*1024)+1)*1024
         total_time = 0
         for i in xrange(10):
             total_time += test_putfile(client)
@@ -99,7 +100,7 @@ if __name__ == '__main__':
 
         file_name = str(each_num)+'Mdeg.txt'
         src_path = base_path+file_name
-        code_info['block_size'] = file_size
+        code_info['block_size'] = file_size/r
         test_putfile(client)
         total_time = 0
         for i in xrange(10):
